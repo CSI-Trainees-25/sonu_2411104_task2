cb94fd1a2ffac0d7b636a4a572450fecb5652e04
Sonu-Sahu <sonukumar15052004@gmail.com>
2025-09-27 15:49:32 +0530

    task-2

--- index.js
+++ index.js
@@ -48,34 +48,34 @@
 let btn = document.querySelector("button");
 let inp = document.querySelector("input");
 
-// create a ul (or select if you already have it in HTML)
+
 let ul = document.createElement("ul");
 document.body.appendChild(ul);
 
 btn.addEventListener("click", function () {
     console.log(inp.value);
 
-    // create list item
+   
     let item = document.createElement("li");
     item.innerText = inp.value;
 
-    // create delete button
+   
     let delbtn = document.createElement("button");
     delbtn.innerText = "Delete";
     delbtn.classList.add("delete");
 
-    // when delete is clicked, remove the li
+    
     delbtn.addEventListener("click", function () {
         item.remove();
     });
 
-    // add delete button inside li
+   
     item.appendChild(delbtn);
 
-    // add li to ul
+    
     ul.appendChild(item);
 
-    // clear input
+    
     inp.value = "";
 });
 
